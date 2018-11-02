a055a71758ef67481d30ec58d76dec34b211fe23
Ryabov Eugene <yr1605@mail.ru>
2018-11-02 17:47:03 +0300

    test

--- src/store/CarsHuge/reducers/index.ts
+++ src/store/CarsHuge/reducers/index.ts
@@ -14,6 +14,8 @@ export const createInitialSate = (): ICarsHuge => ({
     page: 0
 });
 
+
+
 export const carsHugeReducer = (state = createInitialSate(), action: any) => {
     switch (action.type) {
         case "GET_DATA_REQUEST": {
